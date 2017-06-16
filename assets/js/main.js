0184c25978e1a0020dc93b58f8477d925966bbbb
Gabriela Bravo Faúndez <gbravo.visual@gmail.com>
2017-06-15 22:30:53 -0400

    Desarrollo JS parte 2, Drag and Drop y footer del collage.html listo.

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,3 +31,19 @@ boton.addEventListener("click", function() {
 	}
 
 })
+
+//Drag and Drop:
+
+function drag(ev){ 
+	ev.dataTransfer.setData('text', ev.target.id);
+}
+
+function finalDrop(ev){
+	ev.preventDefault(); 
+}
+
+function drop(ev){
+	ev.preventDefault();
+	var dato = ev.dataTransfer.getData('text');
+	ev.target.appendChild(document.getElementById(dato));	
+}
